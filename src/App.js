558559421c8e92d360a42271eb3d7375b8960a69
sonwan001 <sonwan001@gmail.com>
2022-06-25 15:46:06 +0530

    checking link issue

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Profile from './PortfolioContainer/Home/Profile';
 function App() {
   return (
     <div className="App">
+      <h1>Welcome in the world of juice center from Bhutan</h1>
       <Profile/>
     </div>
   );
