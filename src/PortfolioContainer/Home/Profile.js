26414a04a1944eac44a3db68ce63d908c0698052
sonwan001 <sonwan001@gmail.com>
2022-06-25 15:16:28 +0530

    portfolo-project structure

---
+++ src/PortfolioContainer/Home/Profile.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { FaFacebookSquare,FaYoutubeSquare, FaGooglePlusSquare, FaInstagramSquare, FaTwitterSquare } from "react-icons/fa";
+
+function Profile() {
+  return (
+    <div className="profile-container">
+      <div className="profile-paret">
+        <div className="profile-details">
+            <div className="colz">
+                <a href="#">
+                   <FaFacebookSquare className="fa fa-facebook-square"></FaFacebookSquare>                    
+                </a>
+                <a href="#">
+                    <FaGooglePlusSquare className="fa fa-google-plus-square"></FaGooglePlusSquare>
+                </a>
+                <a href="#">
+                    <FaInstagramSquare className="fa fa-instagram"></FaInstagramSquare>
+                </a>
+                <a href="#">
+                    <FaYoutubeSquare className="fa fa-youtube-square"></FaYoutubeSquare>
+                </a>
+                <a href="#">
+                    <FaTwitterSquare className="fa fa-twitter"></FaTwitterSquare>
+                </a>
+            </div>
+        </div>
+      </div> 
+    </div>
+  )
+}
+
+export default Profile;
